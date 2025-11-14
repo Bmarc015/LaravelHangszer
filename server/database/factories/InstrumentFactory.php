68318d49e632a993273663c8f9372e7a379d2a54
MartaBarcell <bartamarcell0@gmail.com>
2025-11-14 10:35:13 +0100

    Faker_Seeder_Kesz

--- server/database/factories/InstrumentFactory.php
+++ server/database/factories/InstrumentFactory.php
@@ -9,6 +9,47 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class InstrumentFactory extends Factory
 {
+
+    protected array $instrumentName = [
+        ['name' => "zongora"],
+        ['name' => "gitár"],
+        ['name' => "hegedű"],
+        ['name' => "brácsa"],
+        ['name' => "cselló"],
+        ['name' => "nagybőgő"],
+        ['name' => "fuvola"],
+        ['name' => "klarinét"],
+        ['name' => "oboa"],
+        ['name' => "fagott"],
+        ['name' => "szaxofon"],
+        ['name' => "trombita"],
+        ['name' => "kürt"],
+        ['name' => "harsona"],
+        ['name' => "tuba"],
+        ['name' => "dob"],
+        ['name' => "marimba"],
+        ['name' => "xilofon"],
+        ['name' => "ütőgardon"],
+        ['name' => "harmonika"],
+        ['name' => "orgona"],
+        ['name' => "szintetizátor"],
+        ['name' => "ukulele"],
+        ['name' => "mandolin"],
+        ['name' => "banjo"],
+        ['name' => "hárfa"],
+        ['name' => "koto"],
+        ['name' => "tambura"],
+        ['name' => "skót duda"],
+        ['name' => "oud"]
+    ];
+
+    protected function withFaker()
+    {
+        // Manuális beállítás az app config felülírására
+        return \Faker\Factory::create('en_EN');
+    }
+
+
     /**
      * Define the model's default state.
      *
@@ -16,8 +57,16 @@ class InstrumentFactory extends Factory
      */
     public function definition(): array
     {
+        $randomInstrument = $this->faker->randomElement($this->instrumentName);
+
         return [
-            //
+            //       
+            'name' => $randomInstrument['name'], 
+            'description' => $this->faker->sentence(),
+            'brand' => $this->faker->company(),
+            'price' => $this->faker->numberBetween(500, 5000), 
+            'quantity' => $this->faker->numberBetween(0, 1000), 
+
         ];
     }
 }
--- server/database/seeders/InstrumentSeeder.php
+++ server/database/seeders/InstrumentSeeder.php
@@ -14,57 +14,9 @@ class InstrumentSeeder extends Seeder
      */
     public function run(): void
     {
-
-
-        //Seedelés tömbbel.
-        // $data = [];
-        // $fileNameCsv = database_path('csv/instruments.csv');
-
-        // $rows = file($fileNameCsv, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-
-        // for ($i = 1; $i < count($rows); $i++) {
-        //     $cols = explode(';', $rows[$i]);
-
-        //     $data[] = [
-        //         'id' => (int)$cols[0],
-        //         'name' => $cols[1],
-        //         'description' => $cols[2],
-        //         'brand' => $cols[3],
-        //         'price' => (int)$cols[4],
-        //         'quantity' => (int)$cols[5]
-        //     ];
-            // }
-
-
-
-            $data = [];
-            $filePath = database_path('csv/instruments.csv');
-            $data = [];
-            $header = []; // Fejlécek tárolására
-
-            if (($handle = fopen($filePath, 'r')) !== false) {
-                // 1. Beolvassuk a fejléceket (ha vannak)
-                $header = fgetcsv($handle, 0, ';');
-
-                // 2. Soronként beolvassuk az adatokat (0 azt jelenti, hogy nincs korlát a beolvasott sorra)
-                while (($cols = fgetcsv($handle, 0, ';')) !== false) {
-                    if (count($header) === count($cols)) {
-                        // Asszociatív tömb létrehozása (jobb olvashatóság!)
-                        $data[] = array_combine($header, $cols);
-                    }
-                }
-                // 3. Zárjuk a fájlt (itt kötelező!)
-                fclose($handle);
-            }
-
-            if (Instrument::count() === 0) {
-                Instrument::factory()->createMany($data);
-            }
-
-
-            
-            // ini_set('memory_limit', '256M');
-            // Instrument::factory()->count(50)->create();
+            Instrument::truncate();
+            ini_set('memory_limit', '256M');
+            Instrument::factory()->count(50)->create();
         }
     }
 
